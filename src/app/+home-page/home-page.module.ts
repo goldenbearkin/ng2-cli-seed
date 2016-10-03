fea4c84dfd6d3ce4dad6edfb08f80e9061ac08a1
jasonhui <huihangkin@gmail.com>
2016-10-03 17:55:31 +0800

    rename homtRouting to Routing

--- src/app/+home-page/home-page.module.ts
+++ src/app/+home-page/home-page.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
-import { homeRouting } from './home-page.routing';
+import { Routing } from './home-page.routing';
 
 @NgModule({
   imports: [
     CommonModule,
-    homeRouting
+    Routing
   ],
   declarations: [HomePageComponent]
 })
--- src/app/+home-page/home-page.routing.ts
+++ src/app/+home-page/home-page.routing.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@angular/router';
 
 import { HomePageComponent } from './home-page.component';
 
-export const homeRouting: ModuleWithProviders = RouterModule.forChild([
+export const Routing: ModuleWithProviders = RouterModule.forChild([
   {
     path: '',
     children: [
